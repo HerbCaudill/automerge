90d685497d6f3548c20f7dc51a87988dc4016da3
HerbCaudill <herb@devresults.com>
2021-05-14 18:17:31 +0200

    export frontend & backend

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,9 @@
-﻿export * from './automerge'
+﻿// TODO this is really clumsy but is just temporary
+import { IBackend, IFrontend } from './types'
+import * as _Backend from '../backend'
+import * as _Frontend from '../frontend'
+export const Backend = _Backend as unknown as IBackend
+export const Frontend = _Frontend as unknown as IFrontend
+
+export * from './automerge'
 export * from './types'
